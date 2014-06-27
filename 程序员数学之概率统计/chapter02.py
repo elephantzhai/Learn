4b257a69dcd96d0472a2406529fe32a2775461b6
elephantzhai <elephantzhai@gmail.com>
2014-06-27 20:42:36 +0800

    python gui

--- 程序员数学之概率统计/chapter02.py
+++ 程序员数学之概率统计/chapter02.py
@@ -1,6 +1,8 @@
 ﻿import thinkstats
 import math
 import survey
+import Pmf
+import operator
 # 2-1-1
 def Punpkin():
 	weights = [1,1,1,3,3,591]
@@ -28,6 +30,24 @@ def stDevOfBirth():
 	notFirstBirthStDev = math.sqrt(thinkstats.Var(notFirstBirth))
 	print "firstBirthStDev",firstBirthStDev,"notFirstBirthStDev",notFirstBirthStDev
 
+# 2-3-1
+def Mode():
+	items = [1,2,3,2,4,1,3,2,4]
+	hist = Pmf.MakeHistFromList(items)
+	maxFreq=0;maxVal=0;
+	for val,freq in hist.Items():
+		if(freq>maxFreq):
+			maxFreq = freq
+			maxVal = val
+
+	print "maxVal",maxVal,"maxFreq",maxFreq
+def Allmodes():
+	items = [1,2,3,2,4,1,3,2,4]
+	hist = Pmf.MakeHistFromList(items)
+	print sorted(hist.Items(),key=operator.itemgetter(1),reverse=True)
+
 if __name__ == '__main__':
-	Punpkin()
-	stDevOfBirth()
+	# Punpkin()
+	# stDevOfBirth()
+	# Mode()
+	Allmodes()
--- 程序员数学之概率统计/test.py
+++ 程序员数学之概率统计/test.py
@@ -1,9 +1,26 @@
 ﻿import thinkstats
-a = [1,2,3,2]
-b = {1:[1,2,3,4],2:[5,6,7,8],3:[9,10,11,12]}
-print sorted(a)[1:-1]
-print thinkstats.Mean(a)
-print thinkstats.Binom(3,2)
-a = a+[4]
-a.append(5)
-print a
+import Pmf
+import matplotlib.pyplot as pyplot
+
+# a = [1,2,3,2]
+# b = {1:[1,2,3,4],2:[5,6,7,8],3:[9,10,11,12]}
+# print sorted(a)[1:-1]
+# print thinkstats.Mean(a)
+# print thinkstats.Binom(3,2)
+# a = a+[4]
+# a.append(5)
+# print a
+
+# c = {7:8,1:2,3:4,5:6}
+# print c.items()
+# print sorted(c.items())
+# print zip(*sorted(c.items()))
+
+hist= Pmf.MakeHistFromList([1,2,2,3,5])
+print hist.Freq(2)
+print hist.Values()
+print hist.Items()
+pyplot.pie([1,2,3])
+pyplot.show()
+
+# vals,freqs
