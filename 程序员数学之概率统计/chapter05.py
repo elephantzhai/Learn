248fc66ab325d3aaa938a4745771195d7d6f0c55
elephantzhai <elephantzhai@gmail.com>
2014-07-15 22:04:14 +0800

    statistic chapter05 5-4

---
+++ 程序员数学之概率统计/chapter05.py
@@ -0,0 +1,19 @@
+﻿import random
+# 5-4
+def MontyHall():
+	numA,numB = 0,0
+	for i in range(10000):
+		index = int(random.random()*3)
+		if index==2:
+			index =1
+
+		if index == 0:
+			numA+=1
+		else:
+			numB+=1
+	print numA,numB
+
+	
+
+if __name__  == '__main__':
+	MontyHall()
