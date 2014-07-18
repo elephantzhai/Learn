2e0bfd64168587ad27cebca03098e1eb69893b48
elephantzhai <elephantzhai@gmail.com>
2014-07-18 12:09:52 +0800

    statistic chapter06 6-1

--- 程序员数学之概率统计/chapter06.py
+++ 程序员数学之概率统计/chapter06.py
@@ -1,4 +1,7 @@
-﻿# 6-1-1
+﻿import survey
+import thinkstats
+import math
+# 6-1-1
 def Skewness(samples):
 	l = len(samples)
 	u = sum(samples)*1.0/l
@@ -8,13 +11,38 @@ def Skewness(samples):
 		tmp1 += (samples[i]-u)**2
 		tmp2 += (samples[i]-u)**3
 	m2,m3 = tmp1/l,tmp2/l
-	g1 = m3/((m2**3)**2)
+	print m2,m3
+	g1 = m3/((m2**3)**0.5)
 	return g1
+# 6-1-2	
+def Skewness2():
+	table = survey.Pregnancies()
+	table.ReadRecords()
+	pregLengths = [record.prglength for record in table.records]
+	print len(pregLengths)
 
-# 6-1-2
+	print Skewness(pregLengths)
+
+# 6-1-3
+def PearsonSkewness():
+	table = survey.Pregnancies()
+	table.ReadRecords()
+	pregLengths = [record.prglength for record in table.records]
+	mean,var = thinkstats.MeanVar(pregLengths)
+	sigma = math.sqrt(var)
+	pregLengths.sort()
+	mid = pregLengths[len(pregLengths)/2]
+	print mean,var,mid
+	print 3*(mean-mid)/sigma
+	
+
 def SkewnessTest():
-	samples = [2,2,3,4,5,6]
-	print Skewness(samples)
+	# samples = [2,2,3,4,5,6]
+	# print Skewness(samples)
+	# Skewness2()
+	# PearsonSkewness()
+	
+
 
 
 
