752887f19cde4b5955933c22de031117053cfc47
elephantzhai <elephantzhai@gmail.com>
2014-08-03 19:04:14 +0800

    statistic chapter08 8-3

--- 程序员数学之概率统计/chapter08.py
+++ 程序员数学之概率统计/chapter08.py
@@ -1,5 +1,6 @@
 import random
 import thinkstats
+import math
 
 #8-1
 def MSE(isAverage = True):
@@ -20,5 +21,58 @@ def MSETest():
 	print MSE()
 	print MSE(isAverage = False)
 
+# 8-2
+def Estimator(isN = True):
+	mu,sigma = 0,1
+	testTime = 1000
+	s = 0
+	itemsNum = 6
+	for i in range(testTime):
+		test = [random.normalvariate(mu,sigma) for j in range(itemsNum)]
+		ss = 0
+		aver = thinkstats.Mean(test)
+		for tmp in test:
+			ss += (tmp-aver)**2
+		if isN:
+			s += ss/itemsNum
+		else:
+			s += ss/(itemsNum-1)
+	return 1.0*s/testTime - sigma
+
+
+def EstimatorTest():
+	print Estimator(isN = True)
+	print Estimator(isN = False)
+
+# 8-3
+def Exponential(isAverage = True):
+	lamada = 2
+	testTime = 1000
+	itemsNum = 6
+	log2 = math.log(2)
+	s = 0
+	for i in range(testTime):
+		test = [random.expovariate(lamada) for j in range(itemsNum)]
+		if isAverage:
+			tlamada = 0
+			for x in test:
+				tlamada += 1.0/x
+			tlamada /= itemsNum
+		else:
+			test.sort()
+			middleNum = (test[2]+test[3])/2
+			tlamada = log2/middleNum
+
+		s += 1.0/(tlamada**2)
+
+	return s*1.0/testTime
+
+def ExponentialDistributionTest():
+	print Exponential(isAverage = True)
+	print Exponential(isAverage = False)
+	
+
 if __name__ == '__main__':
-	MSETest()
+	# MSETest()
+	# EstimatorTest()
+	ExponentialDistributionTest()
