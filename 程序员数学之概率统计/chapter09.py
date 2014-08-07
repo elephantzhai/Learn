87a356ab4ab179e0ad65b9e3fbf3de3f93797830
elephantzhai <elephantzhai@gmail.com>
2014-08-08 01:49:17 +0800

    statistic chapter09 9-1

---
+++ 程序员数学之概率统计/chapter09.py
@@ -0,0 +1,33 @@
+import thinkstats
+import math
+# 9-1
+def Cov(pairs):
+	l1,l2 = zip(*pairs)
+
+	mean1 = thinkstats.Mean(l1)
+	sigma1 = math.sqrt(thinkstats.Var(l1))
+	mean2 = thinkstats.Mean(l2)
+	sigma2 = math.sqrt(thinkstats.Var(l2))
+
+	X,Y = [],[]
+	for i in l1:
+		X.append((i - mean1)/sigma1)
+	for i in l2:
+		Y.append((i - mean2)/sigma2)
+
+	n = len(X)
+	cov = 0
+	for i in range(n):
+		cov += X[i]*Y[i]
+	cov /= n
+
+	return cov
+
+def CovTest():
+	l1 = [1,2,3,4,5,6]
+	l2 = [1,3,5,7,9,11]
+	pairs = zip(l1,l2)
+	print Cov(pairs)
+
+if __name__ == "__main__":
+	CovTest()
