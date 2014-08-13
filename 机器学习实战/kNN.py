50884108a99a40341539be89a2a5c862a07a593e
elephantzhai <elephantzhai@gmail.com>
2014-08-13 22:44:26 +0800

    ml chapter2 2-1

---
+++ 机器学习实战/kNN.py
@@ -0,0 +1,17 @@
+﻿# coding=UTF-8
+from numpy import *
+import operator
+
+def createDataSet():
+	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+	labels = ['A','A','B','B']
+	return group,labels
+def classify0(inX,daraSet,labels,k):
+	daraSetSize = daraSet.shape[0]
+
+def main():
+	group,labels = createDataSet()
+	classify0([0,0],group,labels,3)
+
+if __name__ == "__main__":
+	main()
