563c79b87d7f1642b28429d9dfc1fd2c35edc8a5
elephantzhai <elephantzhai@gmail.com>
2014-08-15 01:42:04 +0800

    ml update

---
+++ 机器学习实战/chap02/kNN.py
@@ -0,0 +1,58 @@
+﻿# coding=UTF-8
+from numpy import *
+import operator
+
+# 2.1
+def createDataSet():
+	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
+	labels = ['A','A','B','B']
+	return group,labels
+def classify0(inX,dataSet,labels,k):
+	dataSetSize = dataSet.shape[0]
+	diffMat = tile(inX,(dataSetSize,1)) - dataSet
+	sqDiffMat = diffMat**2
+	sqDistances = sqDiffMat.sum(axis=1)
+	distances = sqDistances**0.5
+	sortedDistIndicites = distances.argsort()
+	classCount = {}
+	for i in range(k):
+		voteIlabel = labels[sortedDistIndicites[i]]
+		classCount[voteIlabel] = classCount.get(voteIlabel,0)+1
+	sortedClassCount = sorted(classCount.iteritems(),key = operator.itemgetter(1),reverse = True)
+	return sortedClassCount[0][0]
+
+def classifyTest2_1():
+	group,labels = createDataSet()
+	label = classify0([0,0],group,labels,3)
+	print label
+
+# 2.2
+def file2matrix(filename):
+	fr = open(filename)
+	arrayOLines = fr.readlines()
+	numberOfLines = len(arrayOLines)
+	returnMat = zeros((numberOfLines,3))
+	classLabelVector = []
+	index = 0
+	for line in arrayOLines:
+		line  = line.strip()
+		listFromLine = line.split('\t')
+		returnMat[index,:] = listFromLine[0:3]
+		classLabelVector.append(int(listFromLine[-1]))
+		index += 1
+	return returnMat,classLabelVector
+	
+def datingTest():
+	filename = 'datingTestSet.txt'
+	file2matrix(filename)
+
+def main():
+	# classifyTest2_1()
+	datingTest()
+
+def test():
+	str = 'largedoses'
+	# print
+
+if __name__ == "__main__":
+	main()
