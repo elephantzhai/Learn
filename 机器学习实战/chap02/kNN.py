c5f92704cab06a9ab195cb92e902f47a4884c68b
elephantzhai <elephantzhai@gmail.com>
2014-08-16 18:11:54 +0800

    ml chapter02 2-3

--- 机器学习实战/chap02/kNN.py
+++ 机器学习实战/chap02/kNN.py
@@ -3,6 +3,7 @@ from numpy import *
 import operator
 import matplotlib
 import matplotlib.pyplot as plt
+from os import listdir
 # 2.1
 def createDataSet():
 	group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -74,9 +75,49 @@ def datingClassTest():
 	print "error rate:",errorCount*1.0/numTestVecs
 	# datingPlot(datingDataMat,datingLables)
 
+# 2-3
+def img2vector(filename):
+	returnVect = zeros((1,1024))
+	fr = open(filename)
+	for i in range(32):
+		lineStr = fr.readline()
+		for j in range(32):
+			returnVect[0,32*i+j] = int(lineStr[j])
+	return returnVect
+
+def handWritingTest():
+	# testVector = img2vector('testDigits/0_13.txt')
+	# print testVector[0,0:31]
+	hwLabels = []
+	trainingFileList = listdir('trainingDigits')
+	m = len(trainingFileList)
+	trainingMat = zeros((m,1024))
+	for i in range(m):
+		fileNameStr = trainingFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNameStr = int(fileStr.split('_')[0])
+		hwLabels.append(classNameStr)
+		trainingMat[i,:] = img2vector('trainingDigits/%s' % fileNameStr)
+	testFileList = listdir('testDigits')
+	errorCount = 0
+	mTest = len(testFileList)
+	for i in range(mTest):
+		fileNameStr = testFileList[i]
+		fileStr = fileNameStr.split('.')[0]
+		classNameStr = int(fileStr.split('_')[0])
+		vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+		classifierResult = classify0(vectorUnderTest,trainingMat,hwLabels,3)
+		print classifierResult,classNameStr
+		if(classifierResult!=classNameStr):
+			errorCount+=1
+	print "error num:",errorCount,"error rate:",errorCount*1.0/mTest
+
+
 def main():
 	# classifyTest2_1()
-	datingClassTest()
+	# datingClassTest()
+	handWritingTest()
+
 
 if __name__ == "__main__":
 	main()
