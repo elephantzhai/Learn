d8450a7af69d38643caf4453711f7e65d13a8534
elephantzhai <elephantzhai@gmail.com>
2014-08-17 21:19:10 +0800

    ml chapter03 3.1.1

---
+++ 机器学习实战/chap03/trees.py
@@ -0,0 +1,32 @@
+﻿# coding=UTF-8
+from math import log
+
+def creteDataSet():
+	dataSet = [[1,1,'maybe'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no'],]
+	labels = ['no surfacing','flippers']
+	return dataSet,labels
+
+def calcShannonEnt(dataSet):
+	numEntries = len(dataSet)
+	labelCounts = {}
+	for featVec in dataSet:
+		currentLabel = featVec[-1]
+		if currentLabel not in labelCounts.keys():
+			labelCounts[currentLabel] = 0
+		labelCounts[currentLabel] +=1
+	shannonEnt = 0.0
+	for key in labelCounts:
+		prob = float(labelCounts[key])/numEntries
+		shannonEnt -= prob*log(prob,2)
+	return shannonEnt
+
+def shannonTest():
+	dataSet,labels = creteDataSet()
+	print calcShannonEnt(dataSet)
+
+def main():
+	shannonTest()
+
+
+if __name__ == "__main__":
+	main()
