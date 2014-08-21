a19d8709e9fcab3080cd0ae6800098bc396dda97
elephantzhai <elephantzhai@gmail.com>
2014-08-21 22:16:31 +0800

    ml 3.2.1

---
+++ 机器学习实战/chap03/treePlotter.py
@@ -0,0 +1,13 @@
+﻿# coding=UTF-8
+import matplotlib.pyplot as plt
+
+decisionNode = dict(boxstyle = 'sawtooth' ,fc='0.8')
+leafNode = dict(boxstyle='round4',fc='0.8')
+arrow_args = dict(arrowstyle="<-")
+
+def plotNode(nodeTxt)
+
+def main():
+
+if __name__ == "__main__":
+	main()
